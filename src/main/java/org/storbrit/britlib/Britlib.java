7206191650adf53919731d7a2aaa099f6437c13f
storbrit <122255463+storbrit@users.noreply.github.com>
2023-01-11 07:38:44 +0000

    Brand mod and add arrp

---
+++ src/main/java/org/storbrit/britlib/Britlib.java
@@ -0,0 +1,19 @@
+package org.storbrit.britlib;
+
+import net.devtech.arrp.api.RuntimeResourcePack;
+import net.minecraft.util.Identifier;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Britlib implements ModInitializer {
+	public static final String MOD_ID = "britlib";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final RuntimeResourcePack BRITLIB_PACK = RuntimeResourcePack.create(new Identifier(MOD_ID, "pack"));
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	}
+}
